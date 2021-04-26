0f9c6cfbf89648bf7baed7c54c1f9f86c12737d4
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2021-04-25 21:40:40 -0300

    Estrutura inicial do vacine-ja

---
+++ src/test/java/com/ufcg/psoft/vacineja/VacineJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.vacineja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacineJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
