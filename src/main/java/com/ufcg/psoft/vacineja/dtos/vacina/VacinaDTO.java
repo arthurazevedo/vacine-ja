3140c3fe801acb1d98b9160a61663ea1f4f23ccc
Luiz Bonfim <luiz.bonfim.neto@ccc.ufcg.edu.br>
2021-05-02 15:13:54 -0300

    DEV004: Uso do lombok no VacinaDTO

--- src/main/java/com/ufcg/psoft/vacineja/dtos/vacina/VacinaDTO.java
+++ src/main/java/com/ufcg/psoft/vacineja/dtos/vacina/VacinaDTO.java
@@ -1,31 +1,12 @@
 package com.ufcg.psoft.vacineja.dtos.vacina;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class VacinaDTO {
     private String fabricante;
     private int dosesRequeridas;
     private int intervaloEntreDoses;
-
-    public String getFabricante() {
-        return fabricante;
-    }
-
-    public void setFabricante(String fabricante) {
-        this.fabricante = fabricante;
-    }
-
-    public int getDosesRequeridas() {
-        return dosesRequeridas;
-    }
-
-    public void setDosesRequeridas(int dosesRequeridas) {
-        this.dosesRequeridas = dosesRequeridas;
-    }
-
-    public int getIntervaloEntreDoses() {
-        return intervaloEntreDoses;
-    }
-
-    public void setIntervaloEntreDoses(int intervaloEntreDoses) {
-        this.intervaloEntreDoses = intervaloEntreDoses;
-    }
 }
